f019bfeabbc29c925fdc95058416aef59b1a1d15
Richard Brown <rb010101@hotmail.com>
2019-01-01 19:23:02 +0000

    End of section 16

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -12,24 +12,34 @@ namespace DatingApp.API.Data
         public DbSet<User> Users {get; set;}
         public DbSet<Photo> Photos {get; set;}
         public DbSet<Like> Likes {get; set;}
+        public DbSet<Message> Messages {get; set;}
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-             // Primary key
+            // Like
             builder.Entity<Like>()
-                .HasKey(k => new {k.LikerId, k.LikeeId});
-
+                .HasKey(k => new {k.LikerId, k.LikeeId});   // Primary key
             builder.Entity<Like>()
                 .HasOne(u => u.Likee)
                 .WithMany(u => u.Likers)
                 .HasForeignKey(u => u.LikeeId)
                 .OnDelete(DeleteBehavior.Restrict);
-
             builder.Entity<Like>()
                 .HasOne(u => u.Liker)
                 .WithMany(u => u.Likees)
                 .HasForeignKey(u => u.LikerId)
                 .OnDelete(DeleteBehavior.Restrict);
+
+            // Message
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(u => u.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(u => u.MessagesReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+
         }
     }
 }
